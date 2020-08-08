ed9e27035e26f8818d749da64bde8b4bb10b1279
Karen <devkarenc@gmail.com>
2020-08-08 17:12:07 -0400

    Parent to child Dropdown

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Dropdown from './components/Dropdown';
+import Chart from './components/Chart';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Dropdown />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      location: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://api.covidtracking.com/v1/states/current.json')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+  }
+
+  handleChange(event) {
+    this.setState({ location: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.setState({ location: event.target.value });
+    alert('submitted');
+    event.preventDefault();
+  }
+
+  render() {
+    const { isLoaded, items, location } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="App">
+        <Dropdown
+          location={location}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        {/* <Chart stateName={this.items.state} /> */}
+        <ul>
+          {items.map((item) => (
+            <li key={item.state}>
+              {item.state} {item.positive}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
